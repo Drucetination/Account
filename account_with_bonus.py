6908e2ba6c6d0b38eccc13b29bea738c43811df2
Drucetination <drucetination@yandex.ru>
2021-07-29 10:24:01 +0100

    added property decorators and demonstration

--- account_with_bonus.py
+++ account_with_bonus.py
@@ -7,21 +7,25 @@ class AccountWithBonus(account.Account):
     def __init__(self, status, bonus_balance, amount):
         super().__init__(amount)
         self.__status = 'Newcomer'
-        self.__bonus_balance = bonus_balance
+        self.__bonus_balance = 200
 
-    def get_status(self):
+    @property
+    def status(self):
         return self.__status
 
-    def get_bonus_balance(self):
+    @property
+    def bonus_balance(self):
         return self.__bonus_balance
 
-    def set_status(self, status):
+    @status.setter
+    def status(self, status):
         if status in AccountWithBonus.statuses:
             self.__status = status
         else:
             print('The status must be Newcomer, Regular or Expert')
 
-    def set_bonus_balance(self, amount):
+    @bonus_balance.setter
+    def bonus_balance(self, amount):
         if amount >= 0:
             self.__bonus_balance = amount
         else:
--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import account
-import bankclient
+import account_with_bonus as awb
 
 
 # Press the green button in the gutter to run the script.
@@ -16,6 +16,10 @@ if __name__ == '__main__':
     print(acc.balance)
     acc.balance = -2000
     print(acc.balance)
+
+    bonus_acc = awb.AccountWithBonus('Newcomer', 200, 0)
+
+    print(bonus_acc.balance, bonus_acc.bonus_balance, bonus_acc.status)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
